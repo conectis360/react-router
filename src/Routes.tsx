d778766f3455d5a550e68d02ad9501ef73b941f9
James Garcia <conectis360@gmail.com>
2025-03-03 11:50:13 -0300

    feat: using RouteError for the creation of error pages

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ProductsPage } from "./pages/ProductsPage";
 import { ProductPage } from "./pages/ProductPage";
 import App from "./App";
+import { ErrorPage } from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "products",
